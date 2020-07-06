6e52f0aee4cabc31432766957015536c9cf637c6
Frank Martin <Martin.Frank@finnova.com>
2020-07-06 07:26:08 +0200

    added content to objects db

---
+++ src/main/resources/sql/Skill.sql
@@ -0,0 +1,7 @@
+INSERT INTO Skill (name) VALUES ('short blade')
+INSERT INTO Skill (name) VALUES ('long blade')
+
+INSERT INTO Skill (name) VALUES ('sneaking')
+INSERT INTO Skill (name) VALUES ('alchemy')
+
+INSERT INTO Skill (name) VALUES ('tracking')
