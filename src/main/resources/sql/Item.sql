200e2845585d4e052730ba8ccd2284ef25ad7cf6
Frank Martin <Martin.Frank@finnova.com>
2020-07-06 07:26:14 +0200

    added content to objects db

--- src/main/resources/sql/Item.sql
+++ src/main/resources/sql/Item.sql
@@ -1,2 +1,4 @@
-INSERT INTO Item (name, worth, weight) VALUES ('dagger', 11.2, 352.2)
-INSERT INTO Item (name, worth, weight) VALUES ('leather helmet', 13.2, 565.4)
+insert into Item (name, worth, weight) values ('steel dagger', 11.2, 352.2)
+insert into Item (name, worth, weight) values ('leather helmet', 13.2, 565.4)
+insert into Item (name, worth, weight) values ('steel chain mail', 243.2, 6261.3)
+insert into Item (name, worth, weight) values ('leather boots', 16.2, 967.5)
